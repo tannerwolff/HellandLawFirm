c81d2c69e8c034d7a7723ae74b7017a6dd11af53
Tanner Wolff <twolff@dawson.edu>
2023-05-28 16:43:32 -0600

    Initial Commit

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>
+          Wolff Web Design Website starting with Github
+        
+        </h1>
+        
+      </div>
+    );
+  }
+}
